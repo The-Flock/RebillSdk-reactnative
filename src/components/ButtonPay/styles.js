0810441486dd08671b8f6b30fb7ea87b4ad836ad
EzequielMontel <85187076+EzequielMontel@users.noreply.github.com>
2022-05-02 18:22:09 -0300

    some improvements

---
+++ src/components/ButtonPay/styles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const ButtonPayStyles = StyleSheet.create({
+  buttonPay: {
+    borderRadius: 4,
+    height: 36,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 12,
+    flexDirection: 'row',
+  },
+  buttonText: {color: 'white', marginRight: 6},
+  iconPay: {width: 20, height: 20},
+});
+
+export default ButtonPayStyles;
